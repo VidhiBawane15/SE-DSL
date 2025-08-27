c61d69470886c78cef076213154d11c387902534
VidhiBawane15 <bvidhi4vidhi@gmail.com>
2025-08-27 19:51:28 +0530

    Update search.py

--- search.py
+++ search.py
@@ -1,49 +1,29 @@
-# function for linear search
-def Linear_S(number, target):
-    found = False
-    for i in range(len(number)):
-        if target == number[i]:
-            print("Element found at index:", i)
-            found = True
-            break
-    if not found:
-        print("Element not present")
+#list consisting of customer id
+id=[2001,2002,2003,2004,2005,2006,2007,2008,2009,2010]
+target=int(input("enter the id of the customer "))
+#linear search
+print("finding customer id via linear search ")
+found = False
+for i in range(len(id)):
+    if target == id[i]:
+        print("Customer id found ")
+        found = True        
+        break
+if not found:
+    print("Customer id not present")
+#binary search
+print("finding customer id via binary search ")
+low = 0
+high = len(id) - 1
 
-
-# function for binary search (list must be sorted)
-def binary_S(number, target):
-    low = 0
-    high = len(number) - 1
-
-    while low <= high:
-        mid = (low + high) // 2
-        if target == number[mid]:
-            print("Element found at index:", mid)
-            break
-        elif target > number[mid]:
-            low = mid + 1
-        else:
-            high = mid - 1
+while low <= high:
+    mid = (low + high) // 2
+    if target == id[mid]:
+        print("Customer id found ")
+        break
+    elif target > id[mid]:
+        low = mid + 1
     else:
-        print("Element not present")
-
-
-# given array/list
-n = int(input("Enter number of elements you want in the array: "))
-number = []
-for i in range(n):
-    element = int(input(f"Enter element {i + 1}: "))
-    number.append(element)
-
-# user input
-choice = int(input("Enter 1 for linear search\nEnter 2 for binary search: "))
-target = int(input("Enter the element to be searched: "))
-
-# checking user input
-if choice == 1:
-    Linear_S(number, target)
-elif choice == 2:
-    number.sort()  # sorting for binary search
-    binary_S(number, target)
+        high = mid - 1
 else:
-    print("Wrong input")
+    print("Customer id not present")
